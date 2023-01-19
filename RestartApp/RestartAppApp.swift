07e712aacbf1078e1a92b4eacb3f82276a4b1b1a
abdel-elsayedd <abdelrahman.elsayed@digital-tectonics.com>
2023-01-18 19:21:00 -0500

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Abdelrahman Elsayed on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
